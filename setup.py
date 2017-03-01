c39c53401c7bb95cc35991e8e6046d95c85e1f0c
boddumanohar <201451024@iiitvadodara.ac.in>
2017-03-01 16:29:57 +0530

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pdfparse',
+  packages = ['pdfparse'], # this must be the same as the name above
+  version = '0.1',
+  description = 'to parse pdf and search for malicious file',
+  author = 'boddu',
+  author_email = 'b.manu199@icloud.com',
+  url = 'https://github.com/boddumanohar/pdfparse', # use the URL to the github repo
+  download_url = 'https://github.com/boddumanohar/pdfparse/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['viruspdf', 'malicious', 'pdfparse'], # arbitrary keywords
+  classifiers = [],
+)
