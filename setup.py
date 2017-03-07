cf012d16b0af28010265251590b1f154052255e6
boddumanohar <201451024@iiitvadodara.ac.in>
2017-03-07 22:11:02 +0530

    updated pdfparse library

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
-from distutils.core import setup
+import os
+from setuptools import setup
+
+def read(fname):
+	return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(
-  name = 'pdfparse',
-  packages = ['pdfparse'], # this must be the same as the name above
-  version = '0.1',
-  description = 'to parse pdf and search for malicious file',
-  author = 'boddu',
-  author_email = 'b.manu199@icloud.com',
-  url = 'https://github.com/boddumanohar/pdfparse', # use the URL to the github repo
-  download_url = 'https://github.com/boddumanohar/pdfparse/archive/0.1.tar.gz', # I'll explain this in a second
-  keywords = ['viruspdf', 'malicious', 'pdfparse'], # arbitrary keywords
-  classifiers = [],
+name = 'pdfparse',
+packages = ['pdfparse'], # this must be the same as the name above
+version = '1.3',
+description = 'to parse pdf and search for malicious file',
+author = 'boddu',
+author_email = 'b.manu199@icloud.com',
+url = 'https://github.com/boddumanohar/pdfparse', # use the URL to the github repo
+download_url = 'https://github.com/boddumanohar/pdfparse/archive/0.1.tar.gz', # I'll explain this in a second
+keywords = ['viruspdf', 'malicious', 'pdfparse'], # arbitrary keywords
+classifiers = [],
+include_package_data = True,
+entry_points={
+		'console_scripts': [
+			'pdfparse = pdfparse.pdfparse:run',
+		],
+	}
 )
